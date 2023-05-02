000ec67e6cb246c0a648cd308f8c815e1abae943
Eluuuuu <eluu300@gmail.com>
2023-05-02 10:13:40 -0300

    segundo ejercicio

---
+++ listas.js
@@ -0,0 +1,10 @@
+const info = ["Eliana", 22, true, new Date(2001,03,21),
+
+   "libro favorito" , {
+        "titulo": "Mi planta de naranja lima",
+        "autor": "Unknown",
+        "fecha": 25 / 02 / 2000,
+        "url": "hhtps//..."
+    }]
+
+console.log(info)
