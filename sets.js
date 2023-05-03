e62d692fedc9c15cc588641d14762d187ceaef15
Eluuuuu <eluu300@gmail.com>
2023-05-03 18:43:47 -0300

    septimo ejercicio

---
+++ sets.js
@@ -0,0 +1,12 @@
+const nombresFamilia=["Laura", "Oriana", "Ivan", "Amadeo", "Candela"]
+
+const miSet= new Set(nombresFamilia)
+console.log(miSet)
+
+miSet.add("Elu")
+console.log(miSet)
+miSet.add("JavaScript")
+console.log(miSet)
+
+
+
