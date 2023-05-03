810e43f3ed20c3f9af8dbf8c31db32680f9ae851
Eluuuuu <eluu300@gmail.com>
2023-05-02 23:56:47 -0300

    quinto ejercicio

---
+++ numeros.js
@@ -0,0 +1,14 @@
+const altura= 172
+const alturaMetros=1.72
+const peso=62.5
+const resultado= alturaMetros.toFixed()
+console.log(resultado)
+const resultado2= Math.floor(peso)
+console.log(resultado2)
+const maxValor= Number.MAX_VALUE
+console.log(maxValor)
+
+
+
+
+
