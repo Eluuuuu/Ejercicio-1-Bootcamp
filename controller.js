710719b49e819fe58c5a1151ea4d22cc76848174
Eluuuuu <eluu300@gmail.com>
2023-05-04 18:31:04 -0300

    Ejercicio de modulos

---
+++ controller.js
@@ -0,0 +1,8 @@
+export const suma=(a,b)=>{
+return a + b
+} 
+
+export const multiplicate=(a,b)=>{
+return a*b
+} 
+
---
+++ index2.js
@@ -0,0 +1,8 @@
+import { multiplicate,suma } from "./controller.js";
+import chalk from 'chalk';
+
+const sum=suma(1, 2) 
+console.log(chalk.green(sum))
+
+const mul=multiplicate(4, 5)
+console.log(chalk.green(mul))
